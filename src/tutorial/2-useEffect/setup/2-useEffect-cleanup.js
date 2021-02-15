aeb83b4b18a2ac4eeaeb022b74084a7367f09d01
Daryl Jason Lazaro <daryl@docspera.com>
2021-02-15 15:06:01 -0800

    useEffectCleanup

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -4,7 +4,25 @@ import React, { useState, useEffect } from 'react';
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', checkSize);
+    return () => {
+      console.log('cleanup');
+      window.removeEventListener('resize', checkSize);
+    }
+  }, [size]);
+
+  return (
+    <React.Fragment>
+      <h1>Window</h1>
+      <h2>{size} PX</h2>
+    </React.Fragment>
+  );
 };
 
 export default UseEffectCleanup;
