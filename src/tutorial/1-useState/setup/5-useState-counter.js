cd598c1257286bfa7bb411cf3b1f4c9b64f31706
Daryl Jason Lazaro <daryl@docspera.com>
2021-02-15 14:13:03 -0800

    useStateCounter

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -1,7 +1,41 @@
 import React, { useState } from 'react';
 
 const UseStateCounter = () => {
-  return <h2>useState counter example</h2>;
+  const [count, setCount] = useState(0);
+  const decreaseCount = () => {
+    setCount(count - 1);
+  };
+  const resetCount = () => {
+    setCount(0);
+  };
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+  const increaseCountDelay = () => {
+    setTimeout(()=>{
+      //every time you click the increase later button, it will see all those clicks as an increase to the state
+      setCount((prevState)=> {
+        return prevState + 1;
+      })
+    }, 2000);
+  };
+
+  return (
+    <>
+      <div>
+        <h2>Regular Counter</h2>
+        <h1>{count}</h1>
+        <button className="btn" onClick={decreaseCount}>Decrease</button>
+        <button className="btn" onClick={resetCount}>Reset</button>
+        <button className="btn" onClick={increaseCount}>Increase</button>
+      </div>
+      <div style={{marginTop: '3rem'}}>
+        <h2>More Complex Counter</h2>
+        <h1>{count}</h1>
+        <button className="btn" onClick={increaseCountDelay}>Increase Later</button>
+      </div>
+    </>
+  );
 };
 
 export default UseStateCounter;
