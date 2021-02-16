4b731c09b33eea3927af93d3c710b248fcd10b26
Daryl Jason Lazaro <daryl@docspera.com>
2021-02-15 22:21:35 -0800

    multipleInputs

--- src/tutorial/4-forms/setup/2-multiple-inputs.js
+++ src/tutorial/4-forms/setup/2-multiple-inputs.js
@@ -7,24 +7,25 @@ import React, { useState } from 'react';
 // dynamic object keys
 
 const ControlledInputs = () => {
-  const [firstName, setFirstName] = useState('');
-  const [email, setEmail] = useState('');
+  const [person, setPerson] = useState({firstName: '', email: '', age: ''});
   const [people, setPeople] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (firstName && email) {
-      const person = { id: new Date().getTime().toString(), firstName, email };
-      console.log(person);
-      setPeople((people) => {
-        return [...people, person];
-      });
-      setFirstName('');
-      setEmail('');
-    } else {
-      console.log('empty values');
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+
+    setPerson({...person, [name]: value});
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(person.firstName && person.email && person.age){
+      const newPerson = {...person, id: new Date().getTime().toString()};
+      setPeople([...people, newPerson]);
+      setPerson({firstName: '', email: '', age: ''});
     }
   };
+
   return (
     <>
       <article>
@@ -35,8 +36,8 @@ const ControlledInputs = () => {
               type='text'
               id='firstName'
               name='firstName'
-              value={firstName}
-              onChange={(e) => setFirstName(e.target.value)}
+              value={person.firstName}
+              onChange={handleChange}
             />
           </div>
           <div className='form-control'>
@@ -45,8 +46,18 @@ const ControlledInputs = () => {
               type='email'
               id='email'
               name='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              value={person.email}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='form-control'>
+            <label htmlFor='age'>Age : </label>
+            <input
+              type='age'
+              id='age'
+              name='age'
+              value={person.age}
+              onChange={handleChange}
             />
           </div>
           <button type='submit'>add person</button>
